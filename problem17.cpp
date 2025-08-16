90276786ebca6c8fcd32d906d3226afba6b90d52
jlo458 <130149735+jlo458@users.noreply.github.com>
2025-08-16 22:39:58 +0100

    Create problem17.cpp

---
+++ problem17.cpp
@@ -0,0 +1,16 @@
+// Letter combinations
+
+int main() {
+    string digits = "23"; // Not needed
+    
+    vector<string> lets_in_nums = {"abc","def","ghi","jkl","mno","pqr","stu","wxyz",};
+    for (auto let : digits) {
+        //cout << let << "\n";
+
+        int val = atoi(&let) - 1;
+        
+        
+
+    }
+    
+}
